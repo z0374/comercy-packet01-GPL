aa79804d15b3761a49039134d810baa7621a1bb5
z0374 <33524313+z0374@users.noreply.github.com>
2026-06-27 11:41:28 -0300

    Atualizar o config.php

--- src/config/config.php
+++ src/config/config.php
@@ -17,9 +17,7 @@ if (basename(path: __FILE__) == basename(path: $_SERVER["SCRIPT_FILENAME"])) {
 function fetchData(array $apiParams)
 {
     // 1. Resolve o caminho do ficheiro de ambiente
-    $envFile = defined("ROOT_PATH_COMMERCE")
-        ? ROOT_PATH_COMMERCE . "/config/.env"
-        : dirname(ROOT_PATH, 2) . "/config/.env";
+    $envFile = dirname(ROOT_PATH, 2) . "/config/.env";
 
     if (!file_exists($envFile)) {
         $envFile = ROOT_PATH_COMMERCE . "/.env";
